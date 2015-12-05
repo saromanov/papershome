760001c5477e9eb7cc5dbe0f61e08227fbe1c5f7
Sergey Romanov <xxsmotur@gmail.com>
2015-12-05 19:32:48 +0500

    Add new models

--- home/models.py
+++ home/models.py
@@ -2,9 +2,30 @@ from django.db import models
 from django.utils import timezone
 import datetime
 import uuid
+import json
 
 from django.utils.encoding import python_2_unicode_compatible
 from taggit.managers import TaggableManager
+
+@python_2_unicode_compatible
+class CodeLink(models.Model):
+    title = models.CharField(max_length=100, verbose_name=u"title", default='Link to implementation')
+    repolink = models.CharField(max_length=100)
+    usage = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+@python_2_unicode_compatible
+class AdditionalLinks(models.Model):
+    links = models.CharField(max_length=5000)
+
+    def set(self, item):
+        self.links = json.dumps(item)
+
+    def get(self):
+        return json.loads(self.links)
 
 @python_2_unicode_compatible
 class Paper(models.Model):
@@ -16,7 +37,8 @@ class Paper(models.Model):
     version=models.CharField(max_length=5)
     category=models.CharField(max_length=100)
     link = models.CharField(max_length=200)
-    repo = models.ForeignKey("CodeLink")
+    repo = models.ForeignKey(CodeLink, null=True)
+    links = models.ForeignKey(AdditionalLinks, null=True)
     votes = models.IntegerField(default=0)
     rating = models.FloatField(default=0.0)
     pubdate = models.DateTimeField('date published')
@@ -31,15 +53,4 @@ class Paper(models.Model):
 
     def current_version(self):
         return self.version
-
-@python_2_unicode_compatible
-class CodeLink(models.Model):
-    uuid = models.CharField(max_length=100, blank=True, unique=True, default=uuid.uuid4)
-    title = models.CharField(max_length=100, verbose_name=u"title", default='Link to implementation')
-    repolink = models.CharField(max_length=100)
-    usage = models.TextField()
-
-    def __str__(self):
-        return self.title
-
 
