33a90566dd017f35be9e6961487d1c9c082d7c38
Sergey Romanov <xxsmotur@gmail.com>
2015-12-08 23:26:47 +0500

    Update

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^add', views.AddPaperView.as_view(), name='add'),
     url(r'^accounts/login', views.LoginView.as_view(), name='login'),
+    url(r'^accounts/register', views.RegistrationView.as_view(), name='registration'),
     url(r'^papers/(?P<paper_id>[0-9a-z-]+)/$', views.PaperView.as_view(), name='paper_page'),
 ]
